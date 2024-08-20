3dcc8171eb7c47bf6c6d2647650faa7b2a7f01d1
Ayoub060 <140269448+Ayoub060@users.noreply.github.com>
2024-08-20 15:23:06 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+# on calcule : dans le cas d'une opération par exemple une somme
+2+3 # Python calcule le résultat mais n'affiche rien dans la sortie
+
+# le print : on affiche
+
+print(2+3) # Python calcule et on lui demande juste de l'afficher
+# le résultat est en dessous du code
